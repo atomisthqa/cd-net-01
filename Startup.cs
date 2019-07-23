0f9e7d83739763fe3d5a946c695197c88fa3a025
Christian Dupuis <cd@atomist.com>
2019-07-23 13:21:10 +0200

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace aspnetapp  
+namespace aspnetapp   
 {
     public class Startup 
     {
