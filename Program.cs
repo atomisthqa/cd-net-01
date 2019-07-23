f041d6bd919cc68170b1766152611209dcea3096
Christian Dupuis <cd@atomist.com>
2019-07-23 13:10:58 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace aspnetapp    
+namespace aspnetapp     
 {
     public class Program  
     {
